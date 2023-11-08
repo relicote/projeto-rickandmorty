52578c5bae26d19817bc2cd36180faedc9d4fa8e
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-11-08 14:24:25 -0300

    att

--- src/app/shared/components/form-search/form-search.component.ts
+++ src/app/shared/components/form-search/form-search.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router'
 @Component({
   selector: 'app-form-search',
-  templateUrl: './form-search.component.html',
-  styleUrls: ['./form-search.component.scss']
+  template: `
+  <input
+    #inputSearch
+    autofocus
+    type="text"
+    class="form-control-lg"
+    placeholder="Procurar..."
+    (keyup)="onSearch(inputSearch.value)"
+    />
+  `,
+  styles: ['input {width: 100%}'],
 })
 export class FormSearchComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onSearch(value:string){
+    // Validando se o valor inserido pelo usuário é maior que 3 caracteres, SE for, o valor passado será pesquisado pela váriavel VALUE
+
+    if(value && value.length > 3){
+      this.router.navigate(['/character-list'],{
+        queryParams:{q:value}
+      })
+    }
   }
 
 }
