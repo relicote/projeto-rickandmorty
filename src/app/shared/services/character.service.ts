e36cff17694bccde1d9c512b53005df927b2de39
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-11-08 14:47:48 -0300

    created new folder
    Created folder 'services' and inside the file 'character.service.ts'

---
+++ src/app/shared/services/character.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CharacterService {
+
+  constructor() { }
+
+  searchCharacters(){}
+  getDetails(id:number){}
+}
