379ec8edae92fc68301a18b40764d9e66cb338cc
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-11-08 19:15:17 -0300

    Update characters.module.ts

--- src/app/components/pages/characters/characters.module.ts
+++ src/app/components/pages/characters/characters.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> parent of ee47cd3 (att imp e exp de characters.module.ts)
 
 @NgModule({
   declarations: [],
