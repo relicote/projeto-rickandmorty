37ed7a8e848a1848a225f1c705f1a81987937124
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-11-08 13:04:50 -0300

    created new components
    Created characters and shared folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) }, { path: 'character-list', loadChildren: () => import('./components/pages/characters/characters-list/characters-list.module').then(m => m.CharactersListModule) }, { path: 'character-details', loadChildren: () => import('./components/pages/characters/characters-details/characters-details.module').then(m => m.CharactersDetailsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
