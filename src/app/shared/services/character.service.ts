1792f085d343eb6cf38c7672c47836800dadccb9
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-11-08 15:13:29 -0300

    att

--- src/app/shared/services/character.service.ts
+++ src/app/shared/services/character.service.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Character } from '@shared/interface/character.interface';
+import { environment } from '@app/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharacterService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   // PAGE - Filtras as solicitações da API por pagina
   // query - Filtrar todos os personagens pela API ou por pesquisa de usuário
 
-  searchCharacters(query='', page= 1){}
+  searchCharacters(query='', page= 1){
+    return this.http.get<Character[]>
+    (`${environment.baseUrlAPI}/?name=${query}&page=${page}`
+    );
+  }
+
   getDetails(id:number){}
 }
