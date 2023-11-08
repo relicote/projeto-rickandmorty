505483b9c41ccf0bc0485c0c387144806e50b7f8
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-11-08 18:58:51 -0300

    att
    atualizando home.module.ts

--- src/app/components/pages/home/home.module.ts
+++ src/app/components/pages/home/home.module.ts
@@ -4,16 +4,24 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { CharactersModule } from '@characters/characters.module';
+import { CharactersDetailsComponent } from '../characters/characters-details/characters-details.component';
+import { CharactersListComponent } from '../characters/characters-list/characters-list.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    CharactersDetailsComponent,
+    CharactersListComponent,
   ],
   imports: [
     CommonModule,
     HomeRoutingModule,
     CharactersModule,
+  ],
+  exports:[
+    CharactersDetailsComponent,
+    CharactersListComponent
   ]
 })
 export class HomeModule { }
