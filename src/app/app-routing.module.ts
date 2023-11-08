9775e6f65d5f7a174530e00dcfa6ec3e2cbe0422
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-11-08 13:19:20 -0300

    att
    new route to character-details to get ID from API

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
 
+  {
+    path:'',
+    redirectTo: 'home',
+    pathMatch:'full',
+  },
+
 { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule)
 },
 
 { path: 'character-list', loadChildren: () => import('./components/pages/characters/characters-list/characters-list.module').then(m => m.CharactersListModule)
 },
 
-{ path: 'character-details', loadChildren: () => import('./components/pages/characters/characters-details/characters-details.module').then(m => m.CharactersDetailsModule)
+{ path: 'character-details/:id', loadChildren: () => import('./components/pages/characters/characters-details/characters-details.module').then(m => m.CharactersDetailsModule)
 }
 
 ];
