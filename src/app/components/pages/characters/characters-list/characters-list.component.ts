0d6d2f014a854172baf39fd7a87ec15c3ff1c54f
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-11-09 12:12:16 -0300

    att characters-list-component html e ts

--- src/app/components/pages/characters/characters-list/characters-list.component.ts
+++ src/app/components/pages/characters/characters-list/characters-list.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Character } from '@app/shared/interface/character.interface';
+import { CharacterService } from '@app/shared/services/character.service';
+import { take } from 'rxjs/operators';
+
+type RequestInfo ={
+  next:string;
+}
 
 @Component({
   selector: 'app-characters-list',
@@ -7,9 +14,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharactersListComponent implements OnInit {
 
-  constructor() { }
+  characters:Character[] = [];
+
+  info:RequestInfo = {
+    next: null,
+  }
+
+
+  private pageNum=1;
+  private query:string;
+  private hideScrollHeight=200;
+  private showScrollHeight = 500;
+
+  constructor(private characterSvc: CharacterService) { }
 
   ngOnInit(): void {
+    this.getDataFromService();
+  }
+
+  private getDataFromService ():void{
+    this.characterSvc.searchCharacters(this.query, this.pageNum)
+    .pipe(take(1))
+    .subscribe((res:any)=>{
+      console.log('Response ->', res)
+
+      const {info, results} = res;
+      this.characters = [...this.characters, ...results ];
+      this.info = info;
+    })
   }
 
 }
