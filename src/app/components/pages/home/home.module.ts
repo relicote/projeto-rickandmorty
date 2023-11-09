6f91ad6395295cb557925f8da2a4ed0fae9b6a2b
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-11-08 23:48:17 -0300

    att de home.module e characters.module
    backup realizado 100% funcional

--- src/app/components/pages/home/home.module.ts
+++ src/app/components/pages/home/home.module.ts
@@ -3,25 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-import { CharactersModule } from '@characters/characters.module';
-import { CharactersDetailsComponent } from '../characters/characters-details/characters-details.component';
-import { CharactersListComponent } from '../characters/characters-list/characters-list.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent,
-    CharactersDetailsComponent,
-    CharactersListComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule,
-    CharactersModule,
-  ],
-  exports:[
-    CharactersDetailsComponent,
-    CharactersListComponent
+    HomeRoutingModule
   ]
 })
 export class HomeModule { }
