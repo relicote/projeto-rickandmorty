eae5484bc78a46fd95ffa8cfe472caa3da42315b
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-11-09 10:57:23 -0300

    Update characters.module.ts

--- src/app/components/pages/characters/characters.module.ts
+++ src/app/components/pages/characters/characters.module.ts
@@ -1,10 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-<<<<<<< HEAD
-=======
-
->>>>>>> parent of ee47cd3 (att imp e exp de characters.module.ts)
 
 // ESSA COISA QUE TÁ ME MATANDO
 @NgModule({
