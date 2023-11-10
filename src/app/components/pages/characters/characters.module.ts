cc676c7e2c5aeac4e62aded30371679527a5b008
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-11-09 23:59:08 -0300

    att character component

--- src/app/components/pages/characters/characters.module.ts
+++ src/app/components/pages/characters/characters.module.ts
@@ -4,8 +4,9 @@ import {RouterModule} from '@angular/router'
 
 import { CharactersDetailsComponent } from '@characters/characters-details/characters-details.component';
 import { CharactersListComponent } from '@characters/characters-list/characters-list.component';
+import { CharacterComponent } from '@characters/characters.component';
 
-const myComponent = [CharactersDetailsComponent,CharactersListComponent]
+const myComponent = [CharactersDetailsComponent,CharactersListComponent, CharacterComponent]
 
 // ESSA COISA QUE TÁ ME MATANDO
 @NgModule({
